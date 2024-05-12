6e3386e2c935adc74c8af36454af01063bf862a1
Isaac Espinoza <isaac.espinoza.b@usach.cl>
2024-05-11 23:36:37 -0400

    Tda Station y Tda Section

---
+++ TDAsection.pl
@@ -0,0 +1,50 @@
+/* TDA SECTION
+Estructura de lista de elementos [ST1,ST2,DI,CO]
+DOM:    ST1: estacion de metro 1 (VAR TDA station)
+		ST2: estacion de metro 2 (VAR TDA station)
+		DI: Distancia entre estaciones (num)
+		CO: nombre de estacion (num >= 0)
+        SECTION: TDA section de metro (VAR)
+
+PREDICADOS:
+	section(ST1,ST2,DI,CO,SECTION)
+    getStation1Section(SECTION,ST1)
+    getStation2Section(SECTION,ST2)
+    getDistanceSection(SECTION,DI)
+    getCostSection(SECTION,CO)
+    setStation1Section(SECTION,ST1,SECTION)
+    setStation2Section(SECTION,ST2,SECTION)
+    setDistanceSection(SECTION,DI,SECTION)
+    setCostSection(SECTION,CO,SECTION)
+    isSection(SECTION)
+
+METAS PRIMARIAS:
+	section: relacionar elementos de una section en una Variable
+    isSection: Verificar section
+    getStation1Section: Station1 de una section
+    getStation2Section: Station2 de una section
+    getDistanceSection: Ditancia de una section
+    getCostSection: Costo de una section
+    setStation1Section: Section con nueva Station1
+    setStation2Section: Section con nueva Station2
+    setDistanceSection: Section con nueva distancia
+    setCostSection: Section con nuevo costo
+*/
+
+%constructor
+section(Station1,Station2,Distancia,Costo,[Station1,Station2,Distancia,Costo]).
+
+%pertenecia
+isSection(SEC):-section(_,_,_,_,SEC).
+
+%selectores
+getStation1Section(SEC,ST1):-section(ST1,_,_,_,SEC).
+getStation2Section(SEC,ST2):-section(_,ST2,_,_,SEC).
+getDistanceSection(SEC,DIS):-section(_,_,DIS,_,SEC).
+getCostSection(SEC,COST):-section(_,_,_,COST,SEC).
+
+%modificadores
+setStation1Section(SEC,NewST1,NewSEC):-section(_,ST2,DIS,COST,SEC),section(NewST1,ST2,DIS,COST,NewSEC).
+setStation2Section(SEC,NewST2,NewSEC):-section(ST1,_,DIS,COST,SEC),section(ST1,NewST2,DIS,COST,NewSEC).
+setDistanceSection(SEC,NewDIS,NewSEC):-section(ST1,ST2,_,COST,SEC),section(ST1,ST2,NewDIS,COST,NewSEC).
+setCostSection(SEC,NewCOST,NewSEC):-section(ST1,ST2,DIS,_,SEC),section(ST1,ST2,DIS,NewCOST,NewSEC).
---
+++ TDAstation.pl
@@ -0,0 +1,53 @@
+/* TDA STATION
+Estrucura de lista de elementos [ID,Nombre,Tipo,StopTime]
+DOM:    ID: numero de estacion (num)
+		Nombre: nombre de estacion (string)
+        Tipo: tipo de estacion (string de una letra {r: regular, m: mantencion, c: combinacion, t: terminal})
+        StopTime: tiempo de parada del tren en la estacion (num >0)
+        ESTACION: TDA estacion de metro (VAR)
+
+PREDICADOS:
+	station(ID,Nombre,Tipo,StopTime,ESTACION)
+    getIdStation(ESTACION,ID)
+    getNameStation(ESTACION,Nombre)
+    getTypeStation(ESTACION,Tipo)
+    getStopTimeStation(ESTACION,StopTime)
+    setIdStation(ESTACION,ID,ESTACION)
+    setNameStayion(ESTACION,Nombre,ESTACION)
+    setTypeStation(ESTACION,Tipo,ESTACION)
+    setStopTimeStation(ESTACION,StopTime,ESTACION)
+    isStation(ESTACION)
+
+METAS PRIMARIAS:
+	station: relacionar elementos de una estacion en una Variable
+    isStation: Verificar estacion
+    getIdStation: Id de una estacion
+    getNameStayion: Nombre de una estacion
+    getTypeStation: Tipo de estacion
+    getStopTimeStation: StopTime de estacion
+    setIdStation: Estacion con nueva Id
+    setNameStayion: Estacion con nuevo Nombre
+    setTypeStation: Estacion con nuevo Tipo
+    setStopTimeStation: Estacion con nuevo StopTime
+*/
+
+
+
+%constructor
+station(IDST,NombreST,TipoST,StopTimeST,[IDST,NombreST,TipoST,StopTimeST]).
+
+%pertenecia
+isStation(ESTACION):-station(_,_,_,_,ESTACION).
+
+%selectores
+getIdStation(ESTACION,ID):-station(ID,_,_,_,ESTACION).
+getNameStation(ESTACION,NOMBRE):-station(_,NOMBRE,_,_,ESTACION).
+getTypeStation(ESTACION,TIPO):-station(_,_,TIPO,_,ESTACION).
+getStopTimeStation(ESTACION,StopTime):-station(_,_,StopTime,_,ESTACION).
+
+%modificadores
+setIdStation(ESTACION,NewID,NewESTACION):-station(_,Nombre,Tipo,StopT,ESTACION),station(NewID,Nombre,Tipo,StopT,NewESTACION).
+setNameStation(ESTACION,NewName,NewESTACION):-station(ID,_,Tipo,StopT,ESTACION),station(ID,NewName,Tipo,StopT,NewESTACION).
+setTypeStation(ESTACION,NewType,NewESTACION):-station(ID,Nombre,_,StopT,ESTACION),station(ID,Nombre,NewType,StopT,NewESTACION).
+setStopTimetation(ESTACION,NewStopT,NewESTACION):-station(ID,Nombre,Tipo,_,ESTACION),station(ID,Nombre,Tipo,NewStopT,NewESTACION).
+
