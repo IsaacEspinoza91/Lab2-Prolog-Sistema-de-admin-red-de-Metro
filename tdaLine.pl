3af53aabc1668ba7d88f087a64393080692de6aa
Isaac Espinoza <isaac.espinoza.b@usach.cl>
2024-05-13 13:37:53 -0400

    13-05 13:37 TDA pcar (Funcionalidad 9), y ordenar/documentar codigo TDA line

--- tdaLine.pl
+++ tdaLine.pl
@@ -1,17 +1,26 @@
 /* TDA Line
 Estructura de lista de elementos [ID,NOM,RailT,Sections]
-DOM:    ID: Ide de la linea (num)
-		NOM: Nombre de la linea (string)
-		RailT: Tipo de riel de la linea (string)
-		Sections: Lista de TDAs Section (Lista de VAR TDAs Section) *puede ser lista vacia en un inicio*
-        LINE: TDA linea de metro (VAR)
-        LENGTH: largo de line| cantidad de estaciones (VAR)
-        DIS: distancia de line (VAR)
-        COS: costo de line (VAR)
+DOM:
+    ID: Id de la linea (num)
+	NOM: Nombre de la linea (string)
+	RailT: Tipo de riel de la linea (string)
+	Sections: Lista de TDAs Section (Lista de VAR TDAs Section) *puede ser lista vacia en un inicio*
+    LINE: TDA linea de metro (VAR)
+    LENGTH: largo de line| cantidad de estaciones (VAR)
+    DIS: distancia de line (VAR)
+    COS: costo de line (VAR)
+    Section: TDA section (VAR TDA Section)
+    LIS: lista de elementos (Lista)
+    Largo: largo de la lista (VAR, num)
+    DistanceSections: distancia de una lista de secciones (VAR, num)
+    CostoSections: costo de una lista de secciones (VAR, num)
+    NombreStation: nombre de una estacion (string)
+    SectionsSubPath: secciones de un tramo de las secciones de la linea (VAR)
+    ELE: elemento x de una lista (cualquier tipo de dato)
 
 PREDICADOS:
 	line(ID,NOM,RailT,Sections,LINE)
-    isLine(LINE)
+    isLineWithoutVerifications(LINE)
     getIdLine(LINE,ID)
     getNameLine(LINE,NOM)
     getRailTypeLine(LINE,RailT)
@@ -21,10 +30,24 @@ PREDICADOS:
     setRailType(LINE,RailT,LINE)
     setSections(LINE,RailT,LINE)
     lineLength(LINE,LENGTH,DIS,COS)
+    addSectionToSections(Sections,Section,Sections)
+    largoLista(LIS,Largo)
+    distanciaTotal(Sections,DistanceSections)
+    costoTotal(Sections,CostSections)
+    lineSectionLength(LINE,NombreStation,NombreStation,SectionsSubPath,DistanceSections,CostoSections)
+    getSubListaSections(Sections,NombreStation,NombreStation,Sections)
+    lineAddSection(LINE, Section, LINE)
+    listaVacia(LIS)
+    checkSections(Sections)
+    checkAllSectionsConnected(Sections)
+    normalLine(Sections)
+    circularLine(Sections)
+    getFirstList(LIS,ELE)
+    getLastList(LIS,ELE)
 
-METAS:
+METAS PRIMARIAS:
 	line: Relacionar elementos de una line en una variable
-    isLine: Verificar line
+    isLineWithoutVerifications: Verificar line sin verificaciones de compatibilidad de sections
     getIdLine: Id de una line
     getNameLine: Name de una line
     getRailTypeLine: RailTpe de una line
@@ -34,20 +57,39 @@ METAS:
     setRailType: Line con nuevo RailType
     setSections: Line con nuevas Sections
     lineLength: Datos largo, distancia, costo de linea
+    lineSectionLength: Datos distancia y costo de un sub tramo de las secciones de una linea
+    lineAddSection: Linea con una sections especificada agregada al final de sections, dado que esta no este repetida
 
 METAS SECUNDARIAS:
     largoLista: numero de elementos de una lista
     distanciaTotal: distancia de una lista de sections
     costoTotal: costo de una lista de sections
+    addSectionToSections: añade una seccion al final de la lista de secciones
+    member: verificar si un elementos esta en una lista
+    getSubListaSections: Sublista de secciones entre dos sections especificadas dados sus nombres
+    listaVacia: verifica si una lista es lista vacia
+    checkSections: determina si los elementos de una lista son efectivamente del tipo TDA section
+    chekAllStationConnected: verifica que todas las sections esten conectadas, es decir, que de una se pueda llegar a todas.
+    normalLine: deterna si las sections de una linea cumplen criterio linea normal, es decir, la primera st1 y la ultima st2 son tipo "t"
+    circularLine: determina si las sections de una linea cumplen criterio linea circular, es decir, la priemra st1 y la ultima st2 son la misma
+    getFirstList: selector primer elemento de una lista
+    getLastList: selector ultimo elemento de una lista
+
 */
 :-use_module(tdaSection).
 :-use_module(tdaStation).
 
-%constructor
+
+
+%constructor, Funcionalidad 4
 line(ID,Nombre,RailType,Sections,[ID,Nombre,RailType,Sections]).
 
+
+
 %pertenecia
-%isLine(LINE):-line(_,_,_,_,LINE).
+isLineWithoutVerifications(LINE):-line(_,_,_,_,LINE).
+
+
 
 %selectores
 getIdLine(LINE,ID):-line(ID,_,_,_,LINE).
@@ -55,33 +97,36 @@ getNameLine(LINE,Name):-line(_,Name,_,_,LINE).
 getRailTypeLine(LINE,RailT):-line(_,_,RailT,_,LINE).
 getSectionsLine(LINE,SECTIONS):-line(_,_,_,SECTIONS,LINE).
 
+
+
 %modificadores
 setIdLine(LINE,NewID,NewLINE):-line(_,NOM,RT,Sects,LINE),line(NewID,NOM,RT,Sects,NewLINE).
 setNameLine(LINE,NewNOM,NewLINE):-line(ID,_,RT,Sects,LINE),line(ID,NewNOM,RT,Sects,NewLINE).
 setRailTypeLine(LINE,NewRT,NewLINE):-line(ID,NOM,_,Sects,LINE),line(ID,NOM,NewRT,Sects,NewLINE).
 setSectionsLine(LINE,NewSects,NewLINE):-line(ID,NOM,RT,_,LINE),line(ID,NOM,RT,NewSects,NewLINE).
 
-%otros predicados
+
+
+%OTROS PREDICADOS
 addSectionToSections(Secciones,NewSeccion,NewSecciones):-append(Secciones,[NewSeccion],NewSecciones).
+
+
+
 
 /*Funcionalidad 5 */
 lineLength(LINE,LENGTH,Distancia,Costo):-line(_,_,_,Sections,LINE),largoLista(Sections,LENGTH),distanciaTotal(Sections,Distancia),costoTotal(Sections,Costo).
 
-
-
-
-
-
+%obtener cantidad de elementos de una lista
 largoLista([],0):-!.
 largoLista([_|Y],R):-largoLista(Y,R1),R is R1+1.
 
+%obtener la distancia total de los elementos de una lista de sections
 distanciaTotal([],0):-!.
 distanciaTotal([X|Y],T):-getDistanceSection(X,DIS),distanciaTotal(Y,T1),T is T1+DIS.
 
+%obtener el costo total de los elementos de una lista de sections
 costoTotal([],0):-!.
 costoTotal([X|Y],T):-getCostSection(X,COS),costoTotal(Y,T1),T is T1+COS.
-
-
 
 
 
@@ -90,22 +135,14 @@ costoTotal([X|Y],T):-getCostSection(X,COS),costoTotal(Y,T1),T is T1+COS.
 lineSectionLength(LINE,NombreST1,NombreST2,Sections,Distancia,Costo):-line(_,_,_,ListaSections,LINE),getSubListaSections(ListaSections,NombreST1,NombreST2,Sections),
     distanciaTotal(Sections,Distancia),costoTotal(Sections,Costo).
 
-
+%obtener sub lista de secciones entre dos secciones especificas, notamos los predicados para los casos en que se encuentre cualquiera de los dos stations primero
 getSubListaSections(Lista,N1,N2,Sub):-
     append(_,[[[ID, N1, T, Stop],St2,Dis,Cost]|Cola],Lista),
     append(Sub,[[[_, N2, _, _],_,_,_]|_],[[[ID, N1, T, Stop],St2,Dis,Cost]|Cola]),!.
 %caso en que la station2 este primero que la station1 en la lista de sections
 getSubListaSections(Lista,N1,N2,Sub):-
-    append(_,[[[ID, N2, T, Stop],St2,Dis,Cost]|Cola],Lista),%ojo, notamos que agregamos el anterior a st segunda, porque en el anterior termina esta,y así no consideramos el tramo extra
+    append(_,[[[ID, N2, T, Stop],St2,Dis,Cost]|Cola],Lista),%ojo, agregamos el anterior a la st segunda, porque en el anterior termina esta, y así no consideramos el tramo extra
     append(Sub,[[[_, N1, _, _],_,_,_]|_],[[[ID, N2, T, Stop],St2,Dis,Cost]|Cola]),!.
-
-
-
-
-
-
-
-
 
 
 
@@ -118,33 +155,26 @@ lineAddSection(LineBefore, Section, LineAfter):-line(ID,Name,RT,SectionsBefore,L
 
 
 
-
-
-
-
-
-
-
-
-
 /* Funcionalidad 8 */
 isLine(LINE):- line(ID,NombreL,RTL,SectionsL,LINE), number(ID), string(NombreL), string(RTL), checkSections(SectionsL),
     normalLine(SectionsL), checkAllStationsConnected(SectionsL),!. %caso linea normal
 isLine(LINE):- line(ID,NombreL,RTL,SectionsL,LINE), number(ID), string(NombreL), string(RTL), checkSections(SectionsL),
     circularLine(SectionsL), checkAllStationsConnected(SectionsL),!. %caso linea cirucular
 
-% determina si los elementos de una lista son efectivamente del tipo TDA section
-checkSections([]).
-checkSections([Act|Cola]):-isSection(Act),checkSections(Cola),!.
 
+% determina si los elementos de una lista son efectivamente del tipo TDA section
+checkSections(Secciones):-not(listaVacia(Secciones)),checkSectionsAux(Secciones),!. %verifica que la linea efectivamente tenga secciones
+checkSectionsAux([]).
+checkSectionsAux([Act|Cola]):-isSection(Act),checkSectionsAux(Cola).
+
+%verifica si una lista es lista vacia
+listaVacia([]).
 
 %verifica que de una estacion se puede llegar a todas las demas estaciones de una lista de secciones, recordar que sections tiene las secciones ordenadas en un sentido de recorrido
 %comparacion entre el nombre de la seccion anterior y la actual (st2 y st1), por lo que partimos del segundo elementos, para poder considerar el anterior 1
 checkAllStationsConnected([[_,St2Actual,_,_]|COLA]):-checkAllStationsConnected(COLA,St2Actual),!. %notamos el uso de el mismo predicado pero con distinta aridad (especie de anidacion)
 checkAllStationsConnected([],_):-!.
 checkAllStationsConnected([[St1Actual,St2Actual,_,_]|COLA],St2Anterior):- St1Actual == St2Anterior, checkAllStationsConnected(COLA,St2Actual).
-
-
 
 %determina si las secciones de una linea cumplen para ser ser linea normal, esto quiere decir que sus estaciones terminales son tipo t
 normalLine(Sections):-getFirstList(Sections,[[_,_,"t",_],_,_,_]), getLastList(Sections,[_,[_,_,"t",_],_,_]).
@@ -156,3 +186,4 @@ circularLine(Sections):-getFirstList(Sections,[StationPivote,_,_,_]), getLastLis
 getFirstList([X|_],X).
 %selector ultimo elemento de una lista
 getLastList(LIST,X):-reverse(LIST,[X|_]).
+
---
+++ tdaPcar.pl
@@ -0,0 +1,52 @@
+/* TDA pcar
+Abstraccion de un carro/bagon de pasajeros que posteriormente conforman un tren. Pueden ser del tipo terminal (tr) o del tipo central (ct)
+Representacion estructura de lista de elementos [ID,CAPA,MODEL,TYPE]
+DOM:
+    ID: Id del carro (num)
+    CAPA: Capacidad de pasajeros del carro (num)
+    MODEL: Modelo del carro (string)
+    TYPE: Tipo de carro (atomo), {tr, ct}
+    PCAR: TDA pcar carro de metro (VAR)
+
+PREDICADOS:
+    pcar(ID,CAPA,MODEL,TYPE,PCAR)
+    isPcar(PCAR)
+    getIdPcar(PCAR,ID)
+    getCapacityPcar(PCAR,CAPA)
+    getModelPcar(PCAR,MODEL)
+    getTypePcar(PCAR,TYPE)
+    setIdPcar(PCAR,ID,PCAR)
+    setCapacityPcar(PCAR,CAPA,PCAR)
+    setModelPcar(PCAR,MODEL,PCAR)
+    setTypePcar(PCAR,TYPE,PCAR)
+
+METAS PRIMARIAS
+    pcar: Relacionar elementos de un pcar en una variable, constructor
+    isPcar: Verificar pcar
+    getIdPcar: Id de un pcar
+    getCapacityPcar: Capacidad de pasajeros de un pcar
+    getModelPcar: Modelo de un pcar
+    getTypePcar: Tipo de un pcar
+    setIdPcar: Pcar con nueva Id
+    setCapacityPcar: Pcar con nueva capacidad
+    setModelPcar: Pcar con nuevo modelo
+    setTypePcar: Pcar con nuevo tipo
+*/
+
+%constructor, FUNCIONALIDAD 9
+pcar(ID,Capacidad,Modelo,Tipo,[ID,Capacidad,Modelo,Tipo]).
+
+%pertenecia
+isPcar(PCAR):-pcar(_,_,_,_,PCAR).
+
+%selectores
+getIdPcar(PCAR,ID):-pcar(ID,_,_,_,PCAR).
+getCapacityPcar(PCAR,CAPA):-pcar(_,CAPA,_,_,PCAR).
+getModelPcar(PCAR,MOD):-pcar(_,_,MOD,_,PCAR).
+getTypePcar(PCAR,TYPE):-pcar(_,_,_,TYPE,PCAR).
+
+%modificadores
+setIdPcar(PCAR,NewID,NewPCAR):-pcar(_,CA,MO,TY,PCAR),pcar(NewID,CA,MO,TY,NewPCAR).
+setCapacityPcar(PCAR,NewCAP,NewPCAR):-pcar(ID,_,MO,TY,PCAR),pcar(ID,NewCAP,MO,TY,NewPCAR).
+setModelPcar(PCAR,NewMODEL,NewPCAR):-pcar(ID,CA,_,TY,PCAR),pcar(ID,CA,NewMODEL,TY,NewPCAR).
+setTypePcar(PCAR,NewTYPE,NewPCAR):-pcar(ID,CA,MO,_,PCAR),pcar(ID,CA,MO,NewTYPE,NewPCAR).
